d03e2ddf3f5bb035dfe3dfa8106ea2afefa043f7
Rob Dodson <lets.email.rob@gmail.com>
2013-09-07 12:37:33 -0700

    Convert tabs to spaces to comply with 2 space style guide

--- test/test.js
+++ test/test.js
@@ -7,47 +7,47 @@ var durableJsonLint = require('durable-json-lint');
 // we do this because bower does not always create the paths object's keys
 // in the same order. so a pure string to string comparison will break.
 var jsonify = function (str) {
-	var dirtyJson = str.slice(str.indexOf('{'), str.lastIndexOf('}') + 1);
-	var cleanJson = durableJsonLint(dirtyJson).json;
+  var dirtyJson = str.slice(str.indexOf('{'), str.lastIndexOf('}') + 1);
+  var cleanJson = durableJsonLint(dirtyJson).json;
 
-	return JSON.parse(cleanJson);
+  return JSON.parse(cleanJson);
 };
 
 exports.bowerRJS = {
-	wireupComponent: function (test) {
-		test.expect(1);
+  wireupComponent: function (test) {
+    test.expect(1);
 
-		var actual = jsonify(grunt.file.read('tmp/config.js'));
-		var expected = jsonify(grunt.file.read('test/fixtures/config-expected.js'));
-		test.deepEqual(actual, expected, 'should wireup Bower components in RequireJS config');
+    var actual = jsonify(grunt.file.read('tmp/config.js'));
+    var expected = jsonify(grunt.file.read('test/fixtures/config-expected.js'));
+    test.deepEqual(actual, expected, 'should wireup Bower components in RequireJS config');
 
-		test.done();
-	},
-	wireupComponentPathless: function (test) {
-		test.expect(1);
+    test.done();
+  },
+  wireupComponentPathless: function (test) {
+    test.expect(1);
 
-		var actual = jsonify(grunt.file.read('tmp/pathless-config.js'));
-		var expected = jsonify(grunt.file.read('test/fixtures/pathless-config-expected.js'));
-		test.deepEqual(actual, expected, 'should wireup Bower components in RequireJS config without paths');
+    var actual = jsonify(grunt.file.read('tmp/pathless-config.js'));
+    var expected = jsonify(grunt.file.read('test/fixtures/pathless-config-expected.js'));
+    test.deepEqual(actual, expected, 'should wireup Bower components in RequireJS config without paths');
 
-		test.done();
-	},
-	wireupComponentGlobalConfig: function (test) {
-		test.expect(1);
+    test.done();
+  },
+  wireupComponentGlobalConfig: function (test) {
+    test.expect(1);
 
-		var actual = jsonify(grunt.file.read('tmp/global-config.js'));
-		var expected = jsonify(grunt.file.read('test/fixtures/global-config-expected.js'));
-		test.deepEqual(actual, expected, 'should wireup Bower components in RequireJS config');
+    var actual = jsonify(grunt.file.read('tmp/global-config.js'));
+    var expected = jsonify(grunt.file.read('test/fixtures/global-config-expected.js'));
+    test.deepEqual(actual, expected, 'should wireup Bower components in RequireJS config');
 
-		test.done();
-	},
-	wireupComponentBaseUrlConfig: function (test) {
-		test.expect(1);
+    test.done();
+  },
+  wireupComponentBaseUrlConfig: function (test) {
+    test.expect(1);
 
-		var actual = jsonify(grunt.file.read('tmp/baseurl.js'));
-		var expected = jsonify(grunt.file.read('test/fixtures/baseurl-expected.js'));
-		test.deepEqual(actual, expected, 'should wireup Bower components relative to baseUrl');
+    var actual = jsonify(grunt.file.read('tmp/baseurl.js'));
+    var expected = jsonify(grunt.file.read('test/fixtures/baseurl-expected.js'));
+    test.deepEqual(actual, expected, 'should wireup Bower components relative to baseUrl');
 
-		test.done();
-	}
+    test.done();
+  }
 };
