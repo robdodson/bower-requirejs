11ba5bbf65ad3ff54df2c803af08710c665a3ff1
Rob Dodson <lets.email.rob@gmail.com>
2013-10-01 10:16:01 -0700

    Cleanup bin architecture a bit so it's more like yo

--- test/test.js
+++ test/test.js
@@ -21,20 +21,18 @@ describe('BowerRequireJS', function() {
   this.timeout(5000);
 
   it('should exist', function() {
-    var options = { config: 'tmp/config.js', excludes: ['underscore'] };
-    var bowerRequireJS = new BowerRequireJS(options);
+    var args = [];
+    var opts = { config: 'tmp/config.js', excludes: ['underscore'] };
+    var bowerRequireJS = new BowerRequireJS(args, opts);
     should.exist(bowerRequireJS);
   });
 
   describe('config', function() {
     it('should return the expected result', function(done) {
-      var options = { config: 'tmp/config.js', excludes: ['underscore'] };
-      var bowerRequireJS = new BowerRequireJS(options);
-      bowerRequireJS.run(function(err) {
-        if (err) {
-          throw err;
-        }
-
+      var args = [];
+      var opts = { config: 'tmp/config.js', excludes: ['underscore'] };
+      var bowerRequireJS = new BowerRequireJS(args, opts);
+      bowerRequireJS.run(function() {
         var actual = jsonify(fs.readFileSync('tmp/config.js', 'utf-8'));
         var expected = jsonify(fs.readFileSync('test/fixtures/config-expected.js', 'utf-8'));
         actual.should.eql(expected);
@@ -45,13 +43,10 @@ describe('BowerRequireJS', function() {
 
   describe('global-config', function() {
     it('should return the expected result', function(done) {
-      var options = { config: 'tmp/global-config.js', excludes: ['underscore'] };
-      var bowerRequireJS = new BowerRequireJS(options);
-      bowerRequireJS.run(function(err) {
-        if (err) {
-          throw err;
-        }
-
+      var args = [];
+      var opts = { config: 'tmp/global-config.js', excludes: ['underscore'] };
+      var bowerRequireJS = new BowerRequireJS(args, opts);
+      bowerRequireJS.run(function() {
         var actual = jsonify(fs.readFileSync('tmp/global-config.js', 'utf-8'));
         var expected = jsonify(fs.readFileSync('test/fixtures/global-config-expected.js', 'utf-8'));
         actual.should.eql(expected);
@@ -62,13 +57,10 @@ describe('BowerRequireJS', function() {
 
   describe('baseurl', function() {
     it('should return the expected result', function(done) {
-      var options = { config: 'tmp/baseurl.js', excludes: ['underscore'], baseUrl: './' };
-      var bowerRequireJS = new BowerRequireJS(options);
-      bowerRequireJS.run(function(err) {
-        if (err) {
-          throw err;
-        }
-
+      var args = [];
+      var opts = { config: 'tmp/baseurl.js', excludes: ['underscore'], baseUrl: './' };
+      var bowerRequireJS = new BowerRequireJS(args, opts);
+      bowerRequireJS.run(function() {
         var actual = jsonify(fs.readFileSync('tmp/baseurl.js', 'utf-8'));
         var expected = jsonify(fs.readFileSync('test/fixtures/baseurl-expected.js', 'utf-8'));
         actual.should.eql(expected);
@@ -79,13 +71,10 @@ describe('BowerRequireJS', function() {
 
   describe('pathless-config', function() {
     it('should return the expected result', function(done) {
-      var options = { config: 'tmp/pathless-config.js', excludes: ['underscore'] };
-      var bowerRequireJS = new BowerRequireJS(options);
-      bowerRequireJS.run(function(err) {
-        if (err) {
-          throw err;
-        }
-
+      var args = [];
+      var opts = { config: 'tmp/pathless-config.js', excludes: ['underscore'] };
+      var bowerRequireJS = new BowerRequireJS(args, opts);
+      bowerRequireJS.run(function() {
         var actual = jsonify(fs.readFileSync('tmp/pathless-config.js', 'utf-8'));
         var expected = jsonify(fs.readFileSync('test/fixtures/pathless-config-expected.js', 'utf-8'));
         actual.should.eql(expected);
